f62811e4ffbc3578a95f67c42646f805f07b3453
두멍이 <bagjui068@gmail.com>
2025-09-10 18:32:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 96 ms, Memory: 9500 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = parseInt(input[0]);
+
+for (i = 1; i <= N; i++) {
+  console.log("*".repeat(i));
+}
