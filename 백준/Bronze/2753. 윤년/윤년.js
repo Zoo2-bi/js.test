df317e1fd1033025ab3d17b87ea4d5ecd84d3db5
주이 <bagjui068@gmail.com>
2025-11-20 17:41:01 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.js
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
 
-const Year = parseInt(input[0]);
-
-if (Year % 4 == 0 && (Year % 100 != 0 || Year % 400 == 0)) {
-  console.log("1");
+const year = parseInt(input[0]);
+if(year%4==0 && year%100!=0 || year%400==0) {
+    console.log("1");
 } else {
-  console.log("0");
+    console.log("0");
 }
