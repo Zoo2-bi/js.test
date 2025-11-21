bb329b6e9e6a7f46b99a7399b7d3074ea36b2c44
박주이 <bagjui068@gmail.com>
2025-11-21 16:26:08 +0900

    [Bronze V] Title: N 찍기, Time: 3540 ms, Memory: 22248 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.js
+++ 백준/Bronze/2741. N 찍기/N 찍기.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
 
-const N = parseInt(input[0]);
+const N =parseInt(input[0]);
 
-for (i = 1; i <= N; i++) {
-  console.log(i);
+for(let i = 1; i<=N; i++) {
+    console.log(i);
 }
