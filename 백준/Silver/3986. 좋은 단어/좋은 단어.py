d2a0e0a89051aa3e0e9449eff4cc3f76bbd97de2
박주이 <bagjui068@gmail.com>
2026-04-13 21:26:10 +0900

    [Silver IV] Title: 좋은 단어, Time: 100 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+count = 0
+
+for _ in range(n):
+    word = input().strip()
+    stack = []
+
+    for ch in word:
+        if stack and stack[-1] == ch:
+            stack.pop()
+        else:
+            stack.append(ch)
+
+    if not stack:
+        count += 1
+
+print(count)
